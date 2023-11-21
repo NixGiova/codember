b827586b9a3c2585b567d8ff9360776ae6391be7
Nixon Moreno <nixon.moreno@etc-latam.com>
2023-11-21 16:11:28 -0500

    Challenge 03

---
+++ src/2023/challenge-03/index.js
@@ -0,0 +1,13 @@
+import { textInput } from './input.js'
+
+const result = textInput.split('\n').filter((line) => {
+  const split = line.split(':')
+  const policy = split[0].split(' ')
+  const key = split[1].trim()
+  const range = policy[0].split('-')
+  const numberLetter = key
+    .split('')
+    .filter((letra) => letra === policy[1]).length
+  return !(numberLetter >= range[0] && numberLetter <= range[1])
+})
+console.log(result[41])
